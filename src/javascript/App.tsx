973d5b3728f111b446ab678699c88a19eeefe20e
Abid Omar <contact@omarabid.com>
2020-10-14 23:56:49 +0100

    fix(typescript): fix typescript warning

--- src/javascript/App.tsx
+++ src/javascript/App.tsx
@@ -1,10 +1,11 @@
-import React, {Component} from "react";
+import * as React from "react";
+import {Component} from "react";
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-          <h1> Hello, World :) </h1>
+          <h1> Hello, World! </h1>
       </div>
     );
   }
