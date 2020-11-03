b7308b2f99385b7d930c581d7ac15c5b65778f7d
Abid Omar <contact@omarabid.com>
2020-11-03 23:28:46 +0100

    wip(routing): add reach router

--- src/javascript/App.tsx
+++ src/javascript/App.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import {Component} from "react";
+import { Router, Link } from "@reach/router";
+
+const Home = () => (
+    <div>Home</div>
+);
+
+const Terms = () => (
+    <div>Terms</div>
+);
 
 class App extends Component {
   render() {
     return(
       <div className="App">
           <h1>Hello, World! </h1>
+          <Link to="/">Home</Link> 
+          <Link to="/terms-of-service">Terms of Service</Link>
+          <Router>
+              <Home path="/" />
+              <Terms path="/terms-of-service" />
+          </Router>
       </div>
     );
   }
