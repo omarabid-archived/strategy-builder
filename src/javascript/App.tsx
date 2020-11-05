d14f6218bfc0d804cbf03b3a9141c88d174f8992
Abid Omar <contact@omarabid.com>
2020-11-05 05:38:01 +0100

    fix(router): add typescript types

--- src/javascript/App.tsx
+++ src/javascript/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import {Component} from "react";
 import { Router, Link } from "@reach/router";
+import { RouteComponentProps } from "@reach/router";
 
-const Home = () => (
+const Home = (props: RouteComponentProps) => (
     <div>Home</div>
 );
 
-const Terms = () => (
+const Terms = (props: RouteComponentProps) => (
     <div>Terms</div>
 );
 
